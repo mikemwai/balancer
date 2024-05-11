428937d655bd7d00e4a64eb47bbcfcfa21c0314f
mikemwai <Joyllette.nyarangi@strathmore.edu>
2024-05-11 23:14:56 +0300

    Task 4 A-1 and A-2 update

---
+++ load_balancer/load_test2.py
@@ -0,0 +1,44 @@
+import asyncio
+import aiohttp
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+async def make_request(session, url):
+    start_time = time.time()
+    async with session.get(url) as response:
+        await response.text()
+    end_time = time.time()
+    time_taken = end_time - start_time
+    return time_taken
+
+
+async def main():
+    url = 'http://localhost:5001/home'
+    average_loads = []
+    num_requests = 10000
+
+    for N in range(2, 7):
+        total_time_taken = 0
+
+        async with aiohttp.ClientSession() as session:
+            tasks = [make_request(session, url) for _ in range(num_requests)]
+            times = await asyncio.gather(*tasks)
+            total_time_taken += sum(times)
+
+        average_load = total_time_taken / num_requests
+        average_loads.append(average_load)
+
+        print(f'Average Load for {num_requests} Requests with {N} server containers: {average_load}')
+
+    # Plot the average load against the number of server containers
+    plt.plot(range(2, 7), average_loads)
+    plt.xlabel('Number of Server Containers')
+    plt.ylabel('Average Load')
+    plt.title('Average Load vs Number of Server Containers')
+    plt.show()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
