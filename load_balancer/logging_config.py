1693fa82f7389713e8290200554e6c6fb47dbdde
adoyohannah <hannah.adoyo@strathmore.edu>
2024-06-12 23:38:59 +0300

    logging_config.py

---
+++ load_balancer/logging_config.py
@@ -0,0 +1,37 @@
+import logging
+import logging.config
+import os
+
+LOGGING_CONFIG = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'formatter': 'standard',
+            'filename': 'load_balancer.log',
+            'mode': 'a',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG',
+            'propagate': True
+        }
+    }
+}
+
+def setup_logging():
+    logging.config.dictConfig(LOGGING_CONFIG)
